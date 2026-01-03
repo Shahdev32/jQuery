c9e30be449a51518d947464cbf31ff5b8cdb42b4
shahdev kumar <shahdevkumar.d@gmail.com>
2026-01-03 22:49:38 +0500

    jquery

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-$("h1").css("color","red");
+// Wait until the HTML document is fully loaded
+$(document).ready(function () {
+
+  // When "Show Box" button is clicked
+  $("#showBtn").click(function () {
+    // Show the box with animation
+    $("#box").fadeIn();
+  });
+
+  // When "Hide Box" button is clicked
+  $("#hideBtn").click(function () {
+    // Hide the box with animation
+    $("#box").fadeOut();
+  });
+
+});
